973dc54bb9b1c68b618204e08552936e87645a27
mestnikoff <165709094+mestnikoff@users.noreply.github.com>
2024-04-02 16:29:05 +0300

    Create get_receipts.py
    Модуль получения кассового чека по его реквизитам (с использованием API-интерфейса сервиса «Проверка чека онлайн»).

---
+++ get_receipts.py
@@ -0,0 +1,22 @@
+import requests
+from settings import proverkacheka_api, proverkacheka_token
+
+
+def get_by_qrraw(qrraw: str) -> dict:
+    data = {'token': proverkacheka_token, 'qrraw': qrraw}
+    response = requests.post(proverkacheka_api, data=data).json()
+    return response
+
+
+def get_by_qrfile(qrfile: str) -> dict:
+    data = {'token': proverkacheka_token}
+    files = {'qrfile': qrfile}
+    response = requests.post(proverkacheka_api, data=data, files=files).json()
+    return response
+
+
+def get_by_qrurl(qrurl: str) -> dict:
+    data = {'token': proverkacheka_token, 'qrurl': qrurl}
+    response = requests.post(proverkacheka_api, data=data).json()
+    return response
+  
